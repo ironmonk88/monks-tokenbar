5935ee957f9689a687ac19751d21790e81918c1c
Jason Seeley <jseeley@seeleyhome.com>
2023-07-29 15:05:36 -0600

    Address case-sensitive compare in skill name to match creation of the key

--- systems/swade-rolls.js
+++ systems/swade-rolls.js
@@ -101,7 +101,7 @@ export class SwadeRolls extends BaseRolls {
             rollfn = actor.rollAttribute;
         }
         else if (request.type == 'skill') {
-            let item = actor.items.find(i => i.name == request.key && i.type == 'skill');
+            let item = actor.items.find(i => MonksTokenBar.slugify(i.name) == request.key && i.type == 'skill');
             sysRequest = item.id;
             rollfn = actor.rollSkill;
         } else {
